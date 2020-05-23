eaa49d4cd307d35e573590dfe47d143cbef26c0e
Nick F <30716635+cybergear791@users.noreply.github.com>
2020-05-23 15:08:27 -0700

    Update Source.cpp
    clean up

--- Source.cpp
+++ Source.cpp
@@ -1,6 +1,6 @@
-//Lab5
-//nick flouty
-//Finds a pass to the destination in a maze.
+// Lab5
+// Nick Flouty
+// Finds a pass to the destination in a maze.
 #include "EasyBMP.h"
 #include "EasyBMP_DataStructures.h"
 #include "EasyBMP_BMP.h"
@@ -20,9 +20,11 @@ bool isBlack(BMP &image, coord x) {
 	return false;
 }
 bool isValid(coord x, BMP &inputMaze) {
-	if (x.x >= inputMaze.TellWidth() ||  x.y >= inputMaze.TellHeight() || x.x < 0 || x.y < 0)		//if start coord is out of picture
+	//if start coord is out of picture
+	if (x.x >= inputMaze.TellWidth() ||  x.y >= inputMaze.TellHeight() || x.x < 0 || x.y < 0)
 		return false;
-	if (isBlack(inputMaze, x))	//if start or finish coord is on a wall
+	//if start or finish coord is on a wall
+	if (isBlack(inputMaze, x))
 		return false;
 	return true;
 }
@@ -38,79 +40,84 @@ void BFS(BMP & I, BMP & O, coord s, coord t, int count) {
 	Q.insert(s,numPixels); count++;
 	//RangedPixelToPixelCopy(I, 0, I.TellWidth(), I.TellHeight(), 0, O, 0, 0);
 	coord p[width][height] = { 0 };
-	bool visited[width][height] = { false };// all vertices;
+	bool visited[width][height] = { false };	// all vertices;
 	visited[s.x][s.y] = true;	//v double array 
-	int d[width][height] = { int INFINITY }; //for all other u?
+	int d[width][height] = { int INFINITY };
 	d[s.x][s.y] = 0;	
 
-	//cout<<"Before While ";
-		while (!Q.isEmpty() && !visited[t.x][t.y]) 
+	while (!Q.isEmpty() && !visited[t.x][t.y]) 
+	{
+		coord u = Q.remove(numPixels);
+		
+		// Check if Up is a valid move
+		coord cUp;
+		cUp.x = u.x;
+		cUp.y = u.y - 1;
+		//above neighbor is a valid point
+		if (isValid(cUp,I) && (!visited[cUp.x][cUp.y]))
 		{
-			//cout<<".";
-			coord u = Q.remove(numPixels);
-
-			coord cUp;	//v
-			cUp.x = u.x;
-			cUp.y = u.y - 1;
-			if (isValid(cUp,I) && (!visited[cUp.x][cUp.y]))	//above neighbor is a valid point)//error check for black pixel & out of bounds
-			{
-					visited[cUp.x][cUp.y] = true;
-				d[cUp.x][cUp.y] = d[u.x][u.y] + 1;
-				p[cUp.x][cUp.y] =u;
-				Q.insert(cUp,numPixels);
-				count++;
-			}//end error checking
-			coord cDown;
-			cDown.x = u.x;
-			cDown.y = u.y + 1;
-			if (isValid(cDown, I) && !visited[cDown.x][cDown.y])
-			{
-					visited[cDown.x][cDown.y] = true;
-				d[cDown.x][cDown.y] = d[u.x][u.y] + 1;
-				p[cDown.x][cDown.y] = u;
-				Q.insert(cDown,numPixels);
-				count++;
-			}//end error checking
-			coord cR;
-			cR.x = u.x+1;
-			cR.y = u.y;
-			if (isValid(cR, I) && !visited[cR.x][cR.y])	
-			{
-					visited[cR.x][cR.y] = true;
-				d[cR.x][cR.y] = d[u.x][u.y] + 1;
-				p[cR.x][cR.y] = u;	
-				Q.insert(cR,numPixels);
-				count++;
-
-			}//end error checking
-			coord cL;
-			cL.x = u.x-1;
-			cL.y = u.y;
-			if (isValid(cL, I) && !visited[cL.x][cL.y])	
-			{
-					visited[cL.x][cL.y] = true;
-				d[cL.x][cL.y] = d[u.x][u.y] + 1;
-				p[cL.x][cL.y] = u;
-				Q.insert(cL,numPixels);
-				count++;
-
-			}//end error checking
-				 // end-for
-		} //end-while
+			visited[cUp.x][cUp.y] = true;
+			d[cUp.x][cUp.y] = d[u.x][u.y] + 1;
+			p[cUp.x][cUp.y] =u;
+			Q.insert(cUp,numPixels);
+			count++;
+		}//end error checking
+		
+		// Check if Down is a valid move
+		coord cDown;
+		cDown.x = u.x;
+		cDown.y = u.y + 1;
+		if (isValid(cDown, I) && !visited[cDown.x][cDown.y])
+		{
+			visited[cDown.x][cDown.y] = true;
+			d[cDown.x][cDown.y] = d[u.x][u.y] + 1;
+			p[cDown.x][cDown.y] = u;
+			Q.insert(cDown,numPixels);
+			count++;
+		}
+	
+		// Check if Right is a valid move
+		coord cR;
+		cR.x = u.x+1;
+		cR.y = u.y;
+		if (isValid(cR, I) && !visited[cR.x][cR.y])	
+		{
+			visited[cR.x][cR.y] = true;
+			d[cR.x][cR.y] = d[u.x][u.y] + 1;
+			p[cR.x][cR.y] = u;	
+			Q.insert(cR,numPixels);
+			count++;
+		}
+		
+		// Check if Left is a valid move
+		coord cL;
+		cL.x = u.x-1;
+		cL.y = u.y;
+		if (isValid(cL, I) && !visited[cL.x][cL.y])	
+		{
+			visited[cL.x][cL.y] = true;
+			d[cL.x][cL.y] = d[u.x][u.y] + 1;
+			p[cL.x][cL.y] = u;
+			Q.insert(cL,numPixels);
+			count++;
+		}
+	} //end-while
+	
 	if (visited[t.x][t.y])
-	{	coord temp = t;
-		while (temp.x != s.x && temp.y != s.y) //v!=s
-			{	//cout<<" .-. ";
-				O(temp.x, temp.y)->Red = 255;
-				O(temp.x, temp.y)->Blue = 0;
-				O(temp.x, temp.y)->Green = 0;
-				temp = p[temp.x][temp.y];	//v=p[v
-			}
-			O.WriteToFile("output.bmp");
-			cout<<endl<< "distance: "<<d[t.x][t.y]<<endl <<"count: "<< count<<endl;
+	{	
+		coord temp = t;
+		while (temp.x != s.x && temp.y != s.y)
+		{
+			O(temp.x, temp.y)->Red = 255;
+			O(temp.x, temp.y)->Blue = 0;
+			O(temp.x, temp.y)->Green = 0;
+			temp = p[temp.x][temp.y];
+		}
+		O.WriteToFile("output.bmp");
+		cout<<endl<< "distance: "<<d[t.x][t.y]<<endl <<"count: "<< count<<endl;
 	}
 	else
-		cout<<"no path from s to t. ";
+		cout<< "no path from s to t. ";
 }
 
 int main() {
@@ -136,22 +143,26 @@ int main() {
 	finish.y = fy;
 
 	bool error = false;
-	if (start.x > inputMaze.TellWidth() && start.y > inputMaze.TellHeight())		//if start coord is out of picture
+	// Check if start coord is out of picture
+	if (start.x > inputMaze.TellWidth() && start.y > inputMaze.TellHeight())
 	{
 		cout << "starting coordinate point is out of range" << endl;
 		error = true;
 	}
-	if (finish.x > inputMaze.TellWidth() && finish.y > inputMaze.TellHeight())	//if finish coord is out of picture
+	// Check if finish coord is out of picture
+	if (finish.x > inputMaze.TellWidth() && finish.y > inputMaze.TellHeight())
 	{
 		cout << "finish coordinate point is out of range" << endl;
 		error = true;
 	}
-	if (isBlack(inputMaze, start) )	//if start coord is on a wall
+	// Check if start coord is on a wall
+	if (isBlack(inputMaze, start)
 	{
 		cout << "Invalid start coordinate point, on maze wall." << endl;
 		error = true;
 	}
-	if (isBlack(inputMaze, finish))	//if finish coord is on a wall
+	// Check if finish coord is on a wall
+	if (isBlack(inputMaze, finish))
 	{
 		cout << "Invalid finish coordinate point, on maze wall." << endl;
 		error = true;
